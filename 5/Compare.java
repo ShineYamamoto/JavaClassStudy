8b149574bf9228a79acbcc885fd2b9f3aec5466d
ShineYamamoto <love.of.240.yen@gmail.com>
2026-08-03 00:41:22 +0900

    Chapter5

---
+++ 5/Compare.java
@@ -0,0 +1,11 @@
+public class Compare {
+    public static void main(String[] args) {
+        int a = 123, b = 456;
+        double c = 10.453;
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a>c);
+    }    
+}

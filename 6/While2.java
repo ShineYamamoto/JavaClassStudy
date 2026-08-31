d21269aa77c6c23ef417516f083af17515e43648
ShineYamamoto <love.of.240.yen@gmail.com>
2026-08-31 11:36:37 +0900

    Chapter6

---
+++ 6/While2.java
@@ -0,0 +1,6 @@
+public class While2 {
+    public static void main(String[] args) {
+        while (Math.random() < 0.8) System.out.println("Win");
+        System.out.println("Lose");
+    }    
+}

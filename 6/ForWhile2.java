d6767fe1969615cb581460adee69392ed92eb8f8
ShineYamamoto <love.of.240.yen@gmail.com>
2026-09-03 01:05:04 +0900

    Chapter6

---
+++ 6/ForWhile2.java
@@ -0,0 +1,9 @@
+public class ForWhile2 {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 1000) {
+            System.out.println(x);
+            x *= 2;
+        }
+    }    
+}
